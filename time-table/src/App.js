7f371e5f87a2f9b8582da7227f0447c8945b540d
jayPreak <jayesh.preak2003@gmail.com>
2023-04-14 19:20:22 +0530

    jeeez

--- time-table/src/App.js
+++ time-table/src/App.js
@@ -1,42 +1,45 @@
-import './App.css';
+import "./App.css";
 
 // Example of a data array that
 // you might receive from an API
 const data = [
-{ name: "28 Nov Mon", age: "SE"},
-{ name: "30 Nov Wed", age: "DAA"},
-{ name: "2 Dec Fri", age: "FDS"},
-{ name: "3 Dec Sat", age: "OE"},
-{ name: "5 Dec Mon", age: "PE"},
-{ name: "7 Dec Wed", age: "CN"},
-]
+  { name: "25 Apr Tue", age: "AI" },
+  { name: "27 Apr Thu", age: "PE" },
+  { name: "28 Apr Fri", age: "OM" },
+  { name: "1 May Mon", age: "OE" },
+  { name: "3 May Wed", age: "Automata" },
+  { name: "5 May Fri", age: "Crypto" },
+];
 
 function App() {
-return (
-	<div className="App">
-	<table>
-		<thead>
-			<tr>
-				<th>Date</th>
-				<th>Exam</th>
-				{/* <th>Gender</th> */}
-			
-			</tr>
-		</thead>
-		{data.map((val, key) => {
-		return (
-			<tbody>
-				<tr key={key}>
-					<td>{val.name}</td>
-					<td>{val.age}</td>
-					{/* <td>{val.gender}</td> */}
-				</tr>
-			</tbody>
-		)
-		})}
-	</table>
-	</div>
-);
+  return (
+    <div className="App">
+      {/* <h1></h1> */}
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Date &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 1:30pm -3:30pm
+            </th>
+            <th>Exam</th>
+            {/* <th>Gender</th> */}
+          </tr>
+        </thead>
+        {data.map((val, key) => {
+          return (
+            <tbody>
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.age}</td>
+                {/* <td>{val.gender}</td> */}
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default App;
